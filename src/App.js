55a0c8e2d9285886fc5301bd20d3d045cdb0582d
Dostonbek <mardonovdoston69@gmail.com>
2022-05-16 17:12:58 +0300

    tuliq

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./containers/Header";
+import ProductDetails from "./containers/ProductDetails";
+import ProductListing from "./containers/ProductListing";
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+//WEBdasturlash yo'nalishi
+//HTML/CSS
+//JavaScript
+//PHP / Python / Java / Ruby / Go
+//Mobile Development
+//Java va Kotlin (Android uchun) 
+//Swift (iOS uchun),
+//Python / JavaScript / C#
+//Game Development
+//Data Science
+//SMM - Social Media Marketing
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductListing = () => {
+  const products = useSelector((state) => state);
+  console.log(products);
   return (
     <div>
       <h1>Product Listing ...</h1>
