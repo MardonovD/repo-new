f4ab824d15d6b767dc4fb5057b8577e16b74549a
Dostonbek <mardonovdoston69@gmail.com>
2022-05-16 09:46:58 +0300

    cefv

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,20 @@
+import { ActionType } from "../contants/action-type";
+
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "doston",
+      category: "Full-Stack developer....",
+    },
+  ],
+};
+
+export const productReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ActionType.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
